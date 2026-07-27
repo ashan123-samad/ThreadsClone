6da39409605927287b63c92be7007db376f73bd3
Ashan Samad <ashansamad300@gmail.com>
2026-07-28 01:52:54 +0500

    Navigation dark theme

--- src/(tabs)/_layout.tsx
+++ src/(tabs)/_layout.tsx
@@ -1,37 +1,20 @@
-import { Tabs } from "expo-router";
-import { Feather } from "@expo/vector-icons";
+import { Slot } from "expo-router";
+import {ThemeProvider, DarkTheme} from "@react-navigation/native";
 
-export default function TabsLayout() {
+const myTheme = {
+  ...DarkTheme,
+  Colors: {
+    ...DarkTheme.colors,
+    primary: 'white',
+  },
+};
 
-    return( <Tabs screenOptions={{tabBarActiveTintColor: 'black', tabBarShowLabel:false}}>
-        <Tabs.Screen name="index" options={{title: 'Home',
-             tabBarIcon: ({size, color}) => (
-         <Feather name="home" size={size} color={color}/> 
-             ),
-            }}
-            />
-        <Tabs.Screen name="search" 
-         options={{title: 'Search',
-             tabBarIcon: ({size, color}) => (
-         <Feather name="search" size={size} color={color}/> 
-             ),
-            }}
-            />
-        <Tabs.Screen name="notifications" 
-         options={{title: 'Notifications',
-             tabBarIcon: ({size, color}) => (
-         <Feather name="heart" size={size} color={color}/> 
-             ),
-            }}
-            />
-        <Tabs.Screen name="profile" 
-         options={{title: 'Profile',
-             tabBarIcon: ({size, color}) => (
-         <Feather name="user" size={size} color={color}/> 
-             ),
-            }}
-            />
-    </Tabs>
 
-    );
+
+export default function RootLayout() {
+  console.log('Root')
+
+  return <ThemeProvider value={myTheme}>
+     <Slot/>
+    </ThemeProvider>
 }
