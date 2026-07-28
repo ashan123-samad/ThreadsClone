cebb87e6d08dc20eda16de264cf9a9dac59c8d3c
Ashan Samad <ashansamad300@gmail.com>
2026-07-28 15:04:12 +0500

    types

--- src/(tabs)/index.tsx
+++ src/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
-import  Button  from "@/components/button";
+
 
 export default function App() {
   return (
